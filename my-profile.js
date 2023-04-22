fe746b9906a98d7eb4b351ad32fc481c965b44cf
ryansummerlin <ryansummerlin777@gmail.com>
2023-04-21 22:34:19 -0700

    Set up the file, created h1 and h2 elements with javascript

---
+++ my-profile.js
@@ -0,0 +1,11 @@
+// console.log("This is my profile page");
+
+const myName = document.createElement("h1");
+myName.innerText = "Ryan Summerlin";
+myName.setAttribute("id", "ryan");
+document.body.appendChild(myName);
+
+const aboutMe = document.createElement("h2");
+aboutMe.innerText = "About Me:";
+aboutMe.setAttribute("id", "about-me");
+document.body.appendChild(aboutMe);
